1feda2d52655aed612453781e287dcde342c2c80
AetherCrusade <nicholashendrata110900@gmail.com>
2020-05-21 09:40:58 +0800

    added all essential features

--- src/SmallScreens.js
+++ src/SmallScreens.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "./Context";
+import { capitalize } from "./Utils";
 import "./SmallScreens.css";
 
 export const GreenScreen = () => {
@@ -9,25 +10,79 @@ export const GreenScreen = () => {
     <div id="green-screen">
       {data.currentPokemon === null ? (
         <span>Select a Pokemon</span>
+      ) : data.details === "error" || data.extraDetails === "error" ? (
+        <span>An Error Occured</span>
+      ) : data.details === null || data.extraDetails === null ? (
+        <span>Loading data...</span>
       ) : (
-        <span>Test</span>
+        <>
+          <span>
+            {"Egg Group: " +
+              (data.extraDetails.egg_groups[0] === null
+                ? "None"
+                : capitalize(data.extraDetails.egg_groups[0].name))}
+          </span>
+          <br />
+          <span>
+            {"Habitat: " +
+              (data.extraDetails.habitat === null
+                ? "None"
+                : capitalize(data.extraDetails.habitat.name))}
+          </span>
+          <br />
+          <span>
+            {"Shape: " +
+              (data.extraDetails.shape === null
+                ? "None"
+                : capitalize(data.extraDetails.shape.name))}
+          </span>
+        </>
       )}
     </div>
   );
 };
 
 export const MiniScreen1 = () => {
+  const [data, setData] = useContext(Context);
+
   return (
     <div id="mini-screen-1">
-      <span>Test</span>
+      {data.currentPokemon === null ? (
+        <span></span>
+      ) : data.details === "error" || data.extraDetails === "error" ? (
+        <span>An Error Occured</span>
+      ) : data.details === null || data.extraDetails === null ? (
+        <span>Loading data...</span>
+      ) : (
+        <>
+          <span>Type: </span>
+          {data.details.types.map((type) => {
+            return <div>{capitalize(type.type.name)}</div>;
+          })}
+        </>
+      )}
     </div>
   );
 };
 
 export const MiniScreen2 = () => {
+  const [data, setData] = useContext(Context);
+
   return (
     <div id="mini-screen-2">
-      <span>Test</span>
+      {data.currentPokemon === null ? (
+        <span></span>
+      ) : data.details === "error" || data.extraDetails === "error" ? (
+        <span>An Error Occured</span>
+      ) : data.details === null || data.extraDetails === null ? (
+        <span>Loading data...</span>
+      ) : (
+        <>
+          <span>{"Height: " + data.details.height / 10 + "m"}</span>
+          <br />
+          <span>{"Weight: " + data.details.weight / 10 + "kg"}</span>
+        </>
+      )}
     </div>
   );
 };
